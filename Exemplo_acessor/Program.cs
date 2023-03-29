e8bdf9e7a475cdd9e247e2edafef9ec92c782a7b
Lucas Santos <100379811+Projetosantos1@users.noreply.github.com>
2023-03-29 16:29:35 -0300

    Add files via upload

---
+++ Exemplo_acessor/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace exemplo_acessor;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto p1 = new Produto();
+        //p1.Preco = 20;
+        p1.AlterarPreco(-50);
+        p1.AlterarNome("PARAFUSO");
+        p1.MostraDados();
+    
+
+
+        //Console.WriteLine("GAveta: " + p1.Preco);
+    }
+}
+ 
